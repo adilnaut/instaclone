862331de4f395412eeb30af410a35f843f548795
Adilet <adilnaut@gmail.com>
2017-06-16 11:17:02 +0600

    Add index, show, edit, destroy to user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,32 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :destroy, :edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update_attributes user_params
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
   def new
     @user = User.new
   end
@@ -17,5 +44,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:login, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
